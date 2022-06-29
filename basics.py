b69ce3e3ff80476f33e6521336cf64e88c0fc358
megharosejayan <megharose15@gmail.com>
2022-06-29 10:37:38 +0900

    basic mouse functions

--- basics.py
+++ basics.py
@@ -3,6 +3,8 @@ import time
 
 time.sleep(3)
 
+#mouse functions
+
 #resolution of the screen
 print(pag.size())
 #current position of the mouse
@@ -11,9 +13,31 @@ print(pag.position())
 #move the mouse over time(3rd argument->no of seconds it takes to do it)
 pag.moveTo(100,100,3)
 
-#move the mouse relative to its current position
+#move the mouse relative to its current position (xcor,ycor,duration-sec)
 pag.moveRel(100,100,3)
 
-#click (xcor,ycor,no.of clicks,duration)
-pag.click(-1189,756,3,3)
-pag.write("10")
+#click (xcor,ycor,no.of clicks,interval between clicks, button)
+pag.click(688,369,1,1,button='left')
+
+pag.write("Hi")
+
+pag.rightClick(x=688, y=369)
+#pag.middleClick(x=moveToX, y=moveToY)
+#pag.doubleClick(x=moveToX, y=moveToY)
+#pag.tripleClick(x=moveToX, y=moveToY)
+
+#scrolls up
+pag.scroll(500)
+#scrolls down
+pag.scroll(-500)
+
+#mouse up and down
+#left click and hold at 500,400
+pag.mouseDown(500,400,button='left')
+#drags to 900,400
+pag.moveTo(900,400)
+#releases the left click
+pag.mouseUp()
+
+
+
